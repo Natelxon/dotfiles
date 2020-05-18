26d368ac5473c6c2e2f67153d7a425fce1be90af
Natelxon <nathan.kift@gmail.com>
2020-05-17 23:10:22 -0700

    background wallpaper

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,5 +8,4 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload top -r&
-    MONITOR=$m polybar --reload bottom -r&
 done
